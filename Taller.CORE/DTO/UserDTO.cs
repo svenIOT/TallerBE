5e60077ac3d15fb735e33678258a25821aaac1be
Daniel Montanez <danielmsmalaga@gmail.com>
2021-01-14 12:56:13 +0100

    Añadir usuario ampliado

--- Taller.CORE/DTO/UserDTO.cs
+++ Taller.CORE/DTO/UserDTO.cs
@@ -6,12 +6,14 @@ namespace Taller.CORE.DTO
 {
     public class UserDTO
     {
+        public int EmployeeId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string Dni { get; set; }
         public string Name { get; set; }
         public string Surnames { get; set; }
         public string Phone { get; set; }
+        public string EmployeeType { get; set; }
 
         public UserDTO()
         {
--- Taller.DAL/Repository/Implements/RepositoryUser.cs
+++ Taller.DAL/Repository/Implements/RepositoryUser.cs
@@ -22,39 +22,82 @@ namespace Taller.DAL.Repository.Implements
 
         public void Add(UserDTO userDTO)
         {
-            // Añadir empleado
-            _context.Empleado.Add(new Empleado()
-                {
-                    Dni = userDTO.Dni,
-                    Usuario = userDTO.Username,
-                    Contrasena = userDTO.Password,
-                    CodConce = 1
-                }
-            );
+            userDTO.EmployeeId = 0; // Autogenerado
+            // Añadir en empleado
+            _addInEmployeeTable(userDTO);
             // Añadir en persona
-            _context.Persona.Add(new Persona()
-                {
-                    Dni = userDTO.Dni,
-                    Nombre = userDTO.Name,
-                    Apellidos = userDTO.Surnames,
-                    Telefono = userDTO.Phone
-                }
-            );
+            _addInPersonTable(userDTO);
+            // Añadir en mecánico o ventas
+            _addInEmployeeTypeTable(userDTO);
+
             _context.SaveChanges();
         }
 
-        public IEnumerable<UserDTO> GetUsers() 
+        public IEnumerable<UserDTO> GetUsers()
         {
             var usersDTO = new List<UserDTO>();
 
             _context.Empleado.ToList().ForEach(u => usersDTO.Add(
-                    new UserDTO 
+                    new UserDTO
                     {
                         Username = u.Usuario,
                         Password = u.Contrasena
                     }
                 ));
             return usersDTO;
+        }
+
+        private void _addInPersonTable(UserDTO userDTO) => _context.Persona.Add(new Persona()
+            {
+                Dni = userDTO.Dni,
+                Nombre = userDTO.Name,
+                Apellidos = userDTO.Surnames,
+                Telefono = userDTO.Phone
+            }
+                );
+
+        private void _addInEmployeeTable(UserDTO userDTO) => _context.Empleado.Add(new Empleado()
+            {
+                CodEmpleado = userDTO.EmployeeId, // Autogenerado
+                Dni = userDTO.Dni,
+                Usuario = userDTO.Username,
+                Contrasena = userDTO.Password,
+                CodConce = 1
+            }
+                );
+
+        private void _addInEmployeeTypeTable(UserDTO userDTO) 
+        {
+            if (userDTO.EmployeeType == "mechanic")
+            {
+                _context.Mecanico.Add(new Mecanico()
+                {
+                    CodMecanico = 0, // Autogenerado
+                    CodMecanicoJefe = 1,
+                    CodEspecialidad = _specialtyToNumber(userDTO.EmployeeType),
+                    CodEmpleado = userDTO.EmployeeId
+                }
+               );
+            } 
+            else
+            {
+                _context.Ventas.Add(new Ventas()
+                {
+                    CodVentas = 0, // Autogenerado
+                    CodEmpleado = userDTO.EmployeeId
+                }
+               );
+            }
+        } 
+        
+        private int _specialtyToNumber(string specialty) 
+        {
+            int specialtyNumber;
+            if (specialty == "car") specialtyNumber = 1;
+            else if (specialty == "motorcycle") specialtyNumber = 2;
+            else specialtyNumber = 3;
+
+            return specialtyNumber;
         }
     }
 }
