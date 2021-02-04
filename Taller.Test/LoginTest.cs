933757a96bc601eb6e3b6465c2b384f4932c5c5e
Daniel Montanez <danielmsmalaga@gmail.com>
2021-02-04 16:30:56 +0100

    Test de integración Login

---
+++ Taller.Test/LoginTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Taller.API;
+using Xunit;
+
+namespace KaynJungle.IntegrationTests
+{
+    public class LoginTests : IClassFixture<TestFixture<Startup>>
+    {
+        private HttpClient Client;
+
+        public LoginTests(TestFixture<Startup> fixture)
+        {
+            Client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task LoginTests_LoginCorrect_ReturnsTrue()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/login",
+                Body = new
+                {
+                    Username = "test",
+                    Password = "test"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value    = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.True(value == "true");
+        }
+
+        [Fact]
+        public async Task LoginTests_LoginIncorrect_ReturnsFalse()
+        {
+            // Arrange
+            var request = new
+            {
+                Url = "/login",
+                Body = new
+                {
+                    Username = "jose",
+                    Password = "magicTheGathering"
+                }
+            };
+
+            // Act
+            var response = await Client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            var value    = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.True(value == "false");
+        }
+
+    }
+}
